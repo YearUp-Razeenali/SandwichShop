951b09f8fb552cf5b5eb9884dbb461724a2f9f0e
Razeenali <razeenali22@gmail.com>
2024-09-30 16:22:04 -0400

    Completed exercise 1 of Sandwich shop

---
+++ src/main/java/com/pluralsight/SandwichShop.java
@@ -0,0 +1,40 @@
+package com.pluralsight;
+
+import java.util.*;
+
+public class SandwichShop {
+
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.print("1: Regular: base price $5.45 \n2: Large: base price $8.95 \nPlease select your sandwich size (1/2): ");
+        int size_option = scanner.nextInt();
+        System.out.print("Please enter your age: ");
+        int age = scanner.nextInt();
+
+        System.out.printf("The cost of your sandwich is: %.2f", sandwich_price(size_option,age));
+
+    }
+
+    public static float sandwich_price(int size, int age){
+        float price;
+        if(size == 1){
+            if(age <= 17){
+               return price = 5.45f * .90f;
+            }else if(age > 17 && age < 65){
+                return price = 5.45f;
+            }else{
+                return price = 5.45f * .80f;
+            }
+        }else{
+            if(age <= 17){
+                return price = 8.95f * .90f;
+            }else if(age > 17 && age < 65){
+                return price = 8.95f;
+            }else{
+                return price = 8.95f * .80f;
+            }
+        }
+
+    }
+}
